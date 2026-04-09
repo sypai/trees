302a61d92bc684969236d3271a083d9bcf287d0e
Suyash Bajpai <suyashbajpai.dev@gmail.com>
2026-04-09 17:30:17 +0530

    construct bst from sorted arrays

---
+++ patterns/bst_invariants/problems/easy_BSTfromSortedArray.py
@@ -0,0 +1,11 @@
+def construct(nums):
+    size = len(nums)
+    if size == 0:
+        return
+    mid = size // 2
+    root = TreeNode(nums[mid])
+
+    root.left = construct(nums[:mid])
+    root.right = construct(nums[mid+1:])
+
+    return root
